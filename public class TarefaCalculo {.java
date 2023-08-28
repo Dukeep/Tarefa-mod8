056a7ed7e1d497723cbc329d09804ef854830795
Dukeep <georgexgame2001@gmail.com>
2023-08-28 19:58:48 -0300

    Projeto corrigido

---
+++ public class TarefaCalculo {.java
@@ -0,0 +1,17 @@
+public class TarefaCalculo {
+    public static void main(String[] args) {
+        Operacoesdivisao();
+    }
+
+    private static void Operacoesdivisao() {
+        System.out.println("*** Operacoes de divisao ***");
+        int num1 = 10;
+        int num2 = 44;
+        int num3 = 4;
+        int num4 = 34;
+        int num5 = (num1 + num2 + num3 + num4) / 4;
+        System.out.println(num5);
+
+
+    }
+}
